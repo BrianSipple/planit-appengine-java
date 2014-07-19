230adc5cb2d37a8672f611091e7ad34b7595f2fe
Brian Sipple <Bsipple57@gmail.com>
2014-07-18 23:52:24 -0500

    Adding shading transitions to header buttons

--- planit/target/planit-1/js/controllers.js
+++ planit/target/planit-1/js/controllers.js
@@ -366,7 +366,14 @@ planitApp.controllers.controller('ShowEventCtrl', function ($scope, $log, oauth2
     $scope.filters = [
     ];
 
-    $scope.$watch($scope.filters, $scope.queryEvents);
+    $scope.$watch($scope.filters, function() {
+
+        if ($scope.filters.length > 0) {
+            $scope.queryEvents();
+        }
+    });
+
+        
 
     $scope.filtereableFields = [
         {enumValue: 'CITY', displayName: 'City'},
@@ -843,7 +850,7 @@ planitApp.controllers.controller('EventDetailCtrl', function ($scope, $log, $rou
 
     $scope.getEventComments = function() {
         $scope.loading = true;
-        gapi.client.planit.getEventReviews( {
+        gapi.client.planit.getEventComments( {
             websafeEventKey: $routeParams.websafeEventKey
         }).execute( function (resp) {
             $scope.$apply( function () {
